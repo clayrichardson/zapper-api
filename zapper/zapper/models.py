8f7bd69515afae68929487f19f57609ff990758f
clay richardson <c.richardson917@gmail.com>
2013-03-09 19:25:10 -0800

    added file, chunk, and transfer models

--- zapper/zapper/models.py
+++ zapper/zapper/models.py
@@ -1,2 +1,23 @@
+
 from django.db import models
+
+HASH_LENGTH = 32
+
+class File(models.Model):
+    uuid = models.CharField(max_length=HASH_LENGTH)
+    name = models.CharField()
+    size = models.IntegerField()
+    owner = models.ForeignKey(User)
+    chunks = models.ForeignKey(FileChunk)
+
+class Chunk(models.Model):
+    uuid = models.CharField(max_length=HASH_LENGTH)
+    chunk_hash = models.CharField(max_length=HASH_LENGTH)
+    validated = modles.BooleanField()
+    reference_count = models.IntegerField()
+
+class Transfer(models.Model):
+    from_user = models.ForeignKey(User)
+    to_user = models.ForeignKey(User)
+    file_id = models.ForeignKey(File)
 
