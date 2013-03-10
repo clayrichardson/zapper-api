0fbf2e485b65c053a77ff80c726da6a2bcdaf0bc
Vinay <vhiremath4@gmail.com>
2013-03-09 19:00:04 -0800

    UserToFile model

--- zapper/zapper/models.py
+++ zapper/zapper/models.py
@@ -1,2 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class UserToFile(models.Model):
+    user = models.ForeignKey(User)
+    file_id = models.ForeignKey(File)
+    class Meta:
+        unique_together = (('user', 'file_id'),)
