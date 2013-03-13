60e3160ef1cbcbd91124cedeb7704e38ffc96e8f
clay richardson <c.richardson917@gmail.com>
2013-03-12 23:02:24 -0700

    changed config.py to postgres settings
    * added postgresql_psycopy2 engine reference
    * added postgresql connection details
    * removed sqlite3 engine reference

--- zapper/zapper/config.py
+++ zapper/zapper/config.py
@@ -3,10 +3,10 @@ import os, sys
 
 APP_PATH = os.path.dirname(__file__)
 
-DATABASE_ENGINE = 'django.db.backends.sqlite3'
+DATABASE_ENGINE = 'django.db.backends.postgresql_psycopg2'
 DATABASE_NAME = 'zapper_db'
-DATABASE_USER = ''
-DATABASE_PASSWORD = ''
-DATABASE_HOST = ''
-DATABASE_PORT = ''
+DATABASE_USER = 'django_login'
+DATABASE_PASSWORD = 'django_login'
+DATABASE_HOST = 'localhost'
+DATABASE_PORT = '5432'
 
