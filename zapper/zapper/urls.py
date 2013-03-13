b26d0a8f1b2d8fe441d4a8711ba003d3d6efdb51
clay richardson <c.richardson917@gmail.com>
2013-03-12 23:16:34 -0700

    added api versioning, api key authentication
    * added ApiKeyResource
    * added url routing for api versioning

--- zapper/zapper/urls.py
+++ zapper/zapper/urls.py
@@ -1,9 +1,17 @@
+
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from tastypie.api import Api
+
+from zapper.api import ApiKeyResource
+
+v1_api = Api(api_name = 'v1')
+v1_api.register(ApiKeyResource())
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login$', 'zapper.end_points.access.login'),
+    url(r'^api/', include(v1_api.urls)),
 )
