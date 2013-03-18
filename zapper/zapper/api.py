4731e2e373e76fbc7a04c6f643d8015c37aa7581
clay richardson <c.richardson917@gmail.com>
2013-03-17 23:32:20 -0700

    modified api.py to use new auth classes

--- zapper/zapper/api.py
+++ zapper/zapper/api.py
@@ -5,7 +5,9 @@ from tastypie.authentication import Authentication
 from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
 
-from zapper.auth import UserAuthentication
+from zapper.auth import ZapperAuthorization
+from zapper.auth import ZapperUserAuthentication
+from zapper.auth import ZapperApiKeyAuthentication
 from zapper.models import *
 
 import logging
@@ -15,20 +17,20 @@ logger = logging.getLogger(__name__)
 class ApiKeyResource(ModelResource):
     class Meta:
         queryset = ApiKey.objects.filter(enabled=True)
-        authentication = UserAuthentication()
+        authentication = ZapperUserAuthentication()
 
 class FileResource(ModelResource):
     class Meta:
         queryset = File.objects.all()
-        authentication = UserAuthentication()
+        authentication = ZapperApiKeyAuthentication()
+        authorization = ZapperAuthorization()
 
     def obj_create(self, bundle, request=None, **kwargs):
         logger.debug('bundle: %s' % (bundle))
         logger.debug('request: %s' % (request))
-        return super(FileResource, self).obj_create(bundle, request, user=request.user)
-
-    def apply_authorization_limits(self, request, object_list):
-        return object_list.filter(user=request.user)
+        return super(FileResource, self).obj_create(
+            bundle, request, user=request.user
+        )
 
 class UserResource(ModelResource):
     class Meta:
