699f8fe4ce52d7696d89263b492dfc341e9e8990
clay richardson <c.richardson917@gmail.com>
2013-03-21 22:21:48 -0700

    eb configuration

--- zapper/zapper/settings.py
+++ zapper/zapper/settings.py
@@ -15,12 +15,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': config.DATABASE_ENGINE,
-        'NAME': config.DATABASE_NAME,
-        'USER': config.DATABASE_USER,
-        'PASSWORD': config.DATABASE_PASSWORD,
-        'HOST': config.DATABASE_HOST,
-        'PORT': config.DATABASE_PORT,
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     }
 }
 
