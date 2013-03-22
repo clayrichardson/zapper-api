f00c07d8d82516c552f5e0f4af2182794fd449ea
Vinay <vhiremath4@gmail.com>
2013-03-21 23:15:04 -0700

    waitlist endpoint working for landing page

--- zapper/zapper/static/js/landing.js
+++ zapper/zapper/static/js/landing.js
@@ -73,10 +73,28 @@ var landing_jobs = {
 			if (validate_email(email)) {
 				$.ajax({
 					type: 'POST',
-					url: '/api/v1/waitlist',
-					dataType: 'json',
-					data: {
-						'email': email
+					url: '/api/v1/waitlist/',
+					contentType: 'application/json',
+					data: '{ "email": "' + email + '"}',
+					beforeSend: function (xhr, settings) {
+						function getCookie(name) {
+							var cookieValue = null;
+							if (document.cookie && document.cookie !== '') {
+								var cookies = document.cookie.split(';');
+								for (var i = 0; i < cookies.length; i++) {
+									var cookie = $.trim(cookies[i]);
+									if (cookie.substring(0, name.length + 1) == (name + '=')) {
+										cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+										break;
+									}
+								}
+							}
+							return cookieValue;
+						}
+
+						if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+							xhr.setRequestHeader('X-CSRFToken', getCookie('csrftoken'));
+						}
 					},
 					success: function (response) {
 						$('.join-form').hide();
--- zapper/zapper/views.py
+++ zapper/zapper/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render_to_response
+from django.views.decorators.csrf import ensure_csrf_cookie
 
+@ensure_csrf_cookie
 def landing_page(request):
     return render_to_response('landing.html', {})
