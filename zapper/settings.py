36f9eb8828554db5765664c924122749a15078aa
clay richardson <c.richardson917@gmail.com>
2013-03-22 00:39:26 -0700

    added STATIC_ROOT for elastic beanstalk

--- zapper/settings.py
+++ zapper/settings.py
@@ -78,6 +78,7 @@ MEDIA_URL = ''
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
 STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'zapper/static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
