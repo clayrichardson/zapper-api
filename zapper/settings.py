13b8f4defa71d446c47b83987c37feda6aae29cb
clay richardson <c.richardson917@gmail.com>
2013-03-22 21:44:11 -0700

    added s3 static urls

--- zapper/settings.py
+++ zapper/settings.py
@@ -77,7 +77,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT  = os.path.join(PROJECT_PATH, '/static/')
+STATIC_ROOT  = ''
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
@@ -87,7 +87,7 @@ AWS_STORAGE_BUCKET_NAME = 'zapperappstatic'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = os.path.join(PROJECT_PATH, 'zapper/static/')
+STATIC_URL = 'http://s3-us-west-1.amazonaws.com/zapperappstatic/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
