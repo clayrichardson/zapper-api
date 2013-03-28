41c7bc8b2bc8225103aec194ac3aacfa00b47aae
clay richardson <c.richardson917@gmail.com>
2013-03-27 18:00:37 -0700

    added admin information to createadmin.py

--- zapper/scripts/createadmin.py
+++ zapper/scripts/createadmin.py
@@ -3,7 +3,11 @@
 from django.contrib.auth.models import User
 
 if User.objects.count() == 0:
-    admin = User.objects.create(username='admin')
+    admin = User.objects.create_user(
+        'clay',
+        'c@zapperapp.com',
+        'Cfa9nTV84pRG32m7Hg647v8qu4fy7RG4AEcZ837PPk2d838kpB'
+    )
     admin.set_password('admin')
     admin.is_superuser = True
     admin.is_staff = True
